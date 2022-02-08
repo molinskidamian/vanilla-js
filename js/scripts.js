2250022a5f4e0c89a4d1fda5ec3a82b686491f5a
Damian Molinski <tedeer@gmail.com>
2022-02-08 22:35:57 +0100

    Better view...

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,17 @@
 	const searchForm = document.querySelector('.search-form');
 	const searchField = document.querySelector('.search-field');
 	const searchBtn = document.querySelector('.search-btn');
+	const query = [];
+
+
 
 	let status = 0;
+
+    showQuery = (e) => {
+		e.preventDefault();
+
+		
+    }
 
 	searchBtn.addEventListener('click', e => {
 		if (status === 0) {
@@ -13,8 +22,12 @@
 			status = 1;
 			console.log(status);
 		} else {
-			console.log('super');
-			e.target.submit;
+			let text = searchField.value;
+			console.log(text);
+			query.push(text);
+			console.log(query);
+
+			console.log('The form has been sent.');
 		}
 	});
 })();
